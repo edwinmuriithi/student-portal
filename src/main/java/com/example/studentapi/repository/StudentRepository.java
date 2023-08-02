d9b67fc4de500f50776efc2241600845b33da56a
edwinmuriithi <kabuimuriithi@gmail.com>
2023-08-02 08:55:33 +0300

    feat(Repository): Edit Student Repository

--- src/main/java/com/example/studentapi/repository/StudentRepository.java
+++ src/main/java/com/example/studentapi/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.example.studentapi.repository;
 
-public interface StudentRepository {
+import com.example.studentapi.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
