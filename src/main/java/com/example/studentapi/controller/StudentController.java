4f716dbc2256ce8c256dd34cadcbb65a1cd077f5
edwinmuriithi <kabuimuriithi@gmail.com>
2023-08-02 09:13:05 +0300

    feat(Controller): Create POST method

--- src/main/java/com/example/studentapi/controller/StudentController.java
+++ src/main/java/com/example/studentapi/controller/StudentController.java
@@ -1,9 +1,31 @@
 package com.example.studentapi.controller;
 
+import com.example.studentapi.entity.Student;
+import com.example.studentapi.repository.StudentRepository;
+import com.example.studentapi.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/students")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public StudentController(StudentService studentService, StudentRepository studentRepository) {
+        this.studentService = studentService;
+        this.studentRepository = studentRepository;
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student){
+        Student newStudent = studentService.createStudent(student);
+        return newStudent;
+    }
 }
