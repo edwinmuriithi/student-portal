a9acf7c786e8dafefa8ef890cb57fbd5369f65ad
edwinmuriithi <kabuimuriithi@gmail.com>
2023-08-02 09:07:23 +0300

    feat(Servie): Add createStudent service implementation

--- src/main/java/com/example/studentapi/controller/StudentController.java
+++ src/main/java/com/example/studentapi/controller/StudentController.java
@@ -1,4 +1,9 @@
 package com.example.studentapi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class StudentController {
 }
--- src/main/java/com/example/studentapi/service/impl/StudentServiceImplementation.java
+++ src/main/java/com/example/studentapi/service/impl/StudentServiceImplementation.java
@@ -1,4 +1,23 @@
 package com.example.studentapi.service.impl;
 
-public class StudentServiceImplementation {
+import com.example.studentapi.entity.Student;
+import com.example.studentapi.repository.StudentRepository;
+import com.example.studentapi.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImplementation implements StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+    @Override
+    public Student createStudent(Student student) {
+        student.setName(student.getName());
+        student.setEmail(student.getEmail());
+        student.setDob(student.getDob());
+        student.setAge(student.getAge());
+        Student newStudent = studentRepository.save(student);
+        return newStudent;
+    }
 }
