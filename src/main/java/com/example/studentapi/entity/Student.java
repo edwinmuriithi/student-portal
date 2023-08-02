1d68569afc9e2ca6475f8c35242a2f25ac023518
edwinmuriithi <kabuimuriithi@gmail.com>
2023-08-02 08:46:59 +0300

    feat(Entity): Create Student Class

--- src/main/java/com/example/studentapi/entity/Student.java
+++ src/main/java/com/example/studentapi/entity/Student.java
@@ -1,6 +1,23 @@
 package com.example.studentapi.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "student")
 public class Student {
 
-
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
 }
