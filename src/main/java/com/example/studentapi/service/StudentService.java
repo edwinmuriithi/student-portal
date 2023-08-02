371145bb0c27d4fff26924d697e1d66d3c82141b
edwinmuriithi <kabuimuriithi@gmail.com>
2023-08-02 08:58:00 +0300

    feat(Servie): Add createStudent method

--- src/main/java/com/example/studentapi/service/StudentService.java
+++ src/main/java/com/example/studentapi/service/StudentService.java
@@ -1,4 +1,10 @@
 package com.example.studentapi.service;
 
+import com.example.studentapi.entity.Student;
+import org.springframework.stereotype.Component;
+
+@Component
 public interface StudentService {
+
+    Student createStudent(Student student);
 }
